e773f7f34915b857e470b0df11bebc5d43f02e65
Michal Kierat <kierate@gmail.com>
2016-08-23 16:56:36 +0100

    Allow executing from global project

---
+++ src/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+$autoloaderPathsToTry = array(
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+);
+
+foreach ($autoloaderPathsToTry as $autoloaderPath) {
+    if (file_exists($autoloaderPath)) {
+        include $autoloaderPath;
+        return true;
+    }
+}
+
+echo "Ellire dependencies have to be installed before using it\n";
+exit;
